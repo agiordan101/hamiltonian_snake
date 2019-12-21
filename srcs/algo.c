47b3bde04d430fcaa2f72a9ff2f828e63f070d52
agiordan <39808296+agiordan101@users.noreply.github.com>
2019-12-21 03:49:04 +0100

    Resout 6x6 dans presque tous les cas !

--- srcs/algo.c
+++ srcs/algo.c
@@ -1,22 +1,15 @@
 #include "snake.h"
 
-//static int		last_chance(t_game *game, t_hamiltonian *cycle, t_node *head, t_node *snake)
-//{
-//}
-
 static int		is_a_choice(t_game *game, int x, int y)
 {
 	if (x == -1 || x == WIDTH ||\
 		y == -1 || y == HEIGHT ||\
+		(game->cycle.cycle_tab[x][y].index < game->cycle.cycle_tab[game->head->x][game->head->y].index &&\
+		(game->cycle.cycle_tab[x][y].index != 1 ||\
+		game->cycle.cycle_tab[game->head->x][game->head->y].index != WIDTH * HEIGHT)) ||\
 		(game->map[x][y] == 1 &&\
-		 &(game->cycle.cycle_tab[x][y]) != game->snake))	//Le dernier membre est a 1 mais est un choix possible !
+		(x != game->snake->x || y != game->snake->y)))	//Le dernier membre est un choix possible !
 		return (0);
-	/*
-	while (snake->next->next)								//avance jusqua la nuque (avant head)
-		snake = snake->next;
-	if (x == snake->x && y == snake->y)
-		return (0);
-	*/
 	return (1);
 }
 
@@ -30,48 +23,47 @@ static int	dist_in_cycle(t_hamiltonian *cycle, int x, int y, int x2, int y2)
 
 static int	best_way(t_game *game, t_hamiltonian *cycle, t_node *head, int search[4])
 {
-	int		best_choice;
+	int		best_dist;
 	int		choice;
 	int		tmp;
 
-	//printf("Lest find best way...\n");
-	best_choice = -1;
-	choice = 0;
+	//printf("Lest find best way... %d %d %d %d\n", search[0], search[1], search[2], search[3]);
+	best_dist = -1;
+	choice = -1;
 	if (search[0] &&\
 		is_a_choice(game, head->x, head->y - 1))
 	{
-		//printf("Dist %d %d");
-		choice = 1;
-		best_choice = dist_in_cycle(cycle, head->x, head->y - 1, game->apple_x, game->apple_y);
-		//*coord = (t_coord){head->x, head->y - 1};
+		choice = 0;
+		best_dist = dist_in_cycle(cycle, head->x, head->y - 1, game->apple_x, game->apple_y);
+		printf("Choice 0 is a choice, dist : %d\n", best_dist);
 	}
 	if (search[1] &&\
 		is_a_choice(game, head->x - 1, head->y) &&\
-		((tmp = dist_in_cycle(cycle, head->x - 1, head->y, game->apple_x, game->apple_y)) < best_choice ||\
-		best_choice == -1))
+		((tmp = dist_in_cycle(cycle, head->x - 1, head->y, game->apple_x, game->apple_y)) < best_dist ||\
+		best_dist == -1))
 	{
-		choice = 2;
-		best_choice = tmp;
-		//*coord = (t_coord){head->x - 1, head->y};
+		choice = 1;
+		best_dist = tmp;
+		printf("Choice 1 is a choice, dist : %d\n", best_dist);
 	}
 	if (search[2] &&\
 		is_a_choice(game, head->x + 1, head->y) &&\
-		((tmp = dist_in_cycle(cycle, head->x + 1, head->y, game->apple_x, game->apple_y)) < best_choice ||\
-		 best_choice == -1))
+		((tmp = dist_in_cycle(cycle, head->x + 1, head->y, game->apple_x, game->apple_y)) < best_dist ||\
+		best_dist == -1))
 	{
-		choice = 3;
-		best_choice = tmp;
-		//*coord = (t_coord){head->x + 1, head->y};
+		choice = 2;
+		best_dist = tmp;
+		printf("Choice 2 is a choice, dist : %d\n", best_dist);
 	}
 	if (search[3] &&\
 		is_a_choice(game, head->x, head->y + 1) &&\
-		(dist_in_cycle(cycle, head->x, head->y + 1, game->apple_x, game->apple_y) < best_choice ||\
-		best_choice == -1))
+		((tmp = dist_in_cycle(cycle, head->x, head->y + 1, game->apple_x, game->apple_y)) < best_dist ||\
+		best_dist == -1))
 	{
-		choice = 4;
-		//*coord = (t_coord){head->x, head->y + 1};
+		choice = 3;
+		best_dist = tmp;
+		printf("Choice 3 is a choice, dist : %d\n", best_dist);
 	}
-	//printf("Current best way is %d\n", choice);
 	return (choice);
 }
 
@@ -96,10 +88,21 @@ static t_node		*find_member(t_game *game, t_hamiltonian *cycle, t_node *new_head
 	(*n_find)++;
 	*dist_member = dist_in_cycle(cycle, new_head->x, new_head->y, tmp->x, tmp->y);
 	//printf("Find new member %d %d index cycle %d\tdist %d\n", tmp->x, tmp->y, tmp->index, *dist_member);
-	member = game->snake;
+	member = game->snake;	//Passe du t_node du cycle au t_node du snake
 	while (member->x != tmp->x || member->y != tmp->y)
 		member = member->next;
 	return (member);
+}
+
+static int		apple_on_road(t_game *game, t_node *new_head, int i_snake)
+{
+	while (new_head->index != i_snake)
+	{
+		if (game->map[new_head->x][new_head->y] == 2)
+			return (1);
+		new_head = new_head->next;
+	}
+	return (0);
 }
 
 static int		crash_snake(t_game *game, t_hamiltonian *cycle, t_node *head, t_node *snake, int dir)
@@ -110,27 +113,31 @@ static int		crash_snake(t_game *game, t_hamiltonian *cycle, t_node *head, t_node
 	int		dist_member;
 
 	//printf("Debut crash snake\n");
-	if (dir == 1)											//
+	if (dir == 0)											//Important pour que le find_member parte de la nouvelle case dans le cycle
 		new_head = &(cycle->cycle_tab[head->x][head->y - 1]);
-	else if (dir == 2)
+	else if (dir == 1)
 		new_head = &(cycle->cycle_tab[head->x - 1][head->y]);
-	else if (dir == 3)
+	else if (dir == 2)
 		new_head = &(cycle->cycle_tab[head->x + 1][head->y]);
 	else
 		new_head = &(cycle->cycle_tab[head->x][head->y + 1]);
 
-	member = find_member(game, cycle, new_head, &n_member, &dist_member);			//Trouve le membre du corps le plus proche
+	member = find_member(game, cycle, new_head, &n_find, &dist_member);			//Trouve le membre du corps le plus proche
 	while (dist_member < snake->index)						//Tant que la distance jusquau membre est plus petite que le snake
 	{
 		//printf("Dist member %d, snake len %d, member index %d\n", dist_member, snake->index, member->index);
-		member = find_member(game, cycle, new_head, &n_member, &dist_member);		//Trouve la prochaine collision a chaque fois
-		if (dist_member <= snake->index - member->index + 1)			//Si la dist pour y aller est plus petite que le nbr de membre jusqua la queue
-		{
-			printf("Crash ! dist %d, len snake %d, member index %d\n", dist_member, snake->index, member->index);
-			return (1);										//de tour pour que la queue s'en aille alors collision
+		member = find_member(game, cycle, new_head, &n_find, &dist_member);		//Trouve la prochaine collision a chaque fois
+		//+1 car jenleve 1 de dist en avancant la tete au debut de la ft()
+		//Si la tete sera colle a la queue ca ne marche pas lorsqu'il y a une pomme sur la route
+		if (dist_member + 1 <= snake->index - member->index ||\
+			(dist_member == snake->index - member->index &&\
+			apple_on_road(game, new_head, cycle->cycle_tab[member->x][member->y].index)))
+		{	
+			printf("Dir %d crash ! Dist member %d, len snake %d, member index %d at %d %d, Apple on road ? %d\n", dir, dist_member, snake->index, member->index, member->x, member->y, apple_on_road(game, new_head, cycle->cycle_tab[member->x][member->y].index));
+			return (1);
 		}
 	}
-	printf("Crash pas dir %d, dist %d,  snake len %d, member index %d\n", dir, dist_member, snake->index, member->index);
+	printf("Dir %d crash pas\n", dir);
 	return (0);
 }
 
@@ -139,21 +146,21 @@ int			algo(t_game *game, t_node *head, t_node *snake)
 	int				search[4];
 	int				dir;
 
-	draw_hamiltonian_cycle(game->cycle.cycle_tab, 0);
-	ft_memset(search, 1, sizeof(int) * 4);
-	search[4 - head->dir] = 0;								//Banni la recherche dans le sens oppose a la dir
+	ft_memset(search, '1', sizeof(int) * 4);
+	search[3 - head->dir] = 0;								//Banni la recherche dans le sens oppose a la dir
+	printf("Sens %d banni car sens inverse\n", 3 - head->dir);
 	dir = best_way(game, &(game->cycle), head, search);		//Chope la dir la plus rapide vers la pomme
 	if (crash_snake(game, &(game->cycle), head, snake, dir))				//Si je m'y crasherai banni la recherche dans cette dir
-		search[dir - 1] = 0;
+		search[dir] = 0;
 	else
 		return (dir);
-	//printf("2eme solution\n");
+	//printf("Sens %d banni\n", dir);
 	dir = best_way(game, &(game->cycle), head, search);		//Reitere pour le 2eme et avant dernier meilleur choix
 	if (crash_snake(game, &(game->cycle), head, snake, dir))
-		search[dir - 1] = 0;
+		search[dir] = 0;
 	else
 		return (dir);
-	//printf("3eme solution\n");
+	//printf("Sens %d banni\n", dir);
 	dir = best_way(game, &(game->cycle), head, search);	//Si les 2 meilleurs ne marche pas prend le plus long
 	if (crash_snake(game, &(game->cycle), head, snake, dir))
 	{
